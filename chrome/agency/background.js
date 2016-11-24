62de95b5fe6185445efe1778cef8ba944bfd18cd
Dong JK <hikihomori@gmail.com>
2016-11-24 15:10:25 +0800

    chrome works

--- chrome/agency/background.js
+++ chrome/agency/background.js
@@ -1,31 +1,30 @@
+function getXMLHTTP() {
+  var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+  xmlhttp.open("POST", "http://localhost:9090/");
+  return xmlhttp
+}
+
 chrome.tabs.onActivated.addListener(function (activeInfo) {
   chrome.tabs.get(activeInfo.tabId, function (tab) {
-
-    var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
-    xmlhttp.open("POST", "http://localhost:9090/");
-    xmlhttp.send(tab.url);
+    getXMLHTTP().send(tab.url);
   })
 
 })
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-
-  var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
-  xmlhttp.open("POST", "http://localhost:9090/");
-  if(changeInfo.url!=null){
-    xmlhttp.send(changeInfo.url);
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.url != null) {
+    getXMLHTTP().send(changeInfo.url);
   }
 });
 
 
 chrome.windows.onFocusChanged.addListener(function (windowId) {
-  var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
-  xmlhttp.open("POST", "http://localhost:9090/");
+  var xmlhttp = getXMLHTTP()
   if (windowId === chrome.windows.WINDOW_ID_NONE) {
     xmlhttp.send("losss all focus");
   } else {
-    chrome.tabs.query({active: true, windowId:windowId}, function (tabs){
-        xmlhttp.send(tabs[0].url);
+    chrome.tabs.query({ active: true, windowId: windowId }, function (tabs) {
+      xmlhttp.send(tabs[0].url);
     })
   }
 })
