9943d6e93a699b1202af7339800f313e089faa2d
Dong JK <hikihomori@gmail.com>
2016-11-24 15:07:27 +0800

    chrome works

--- chrome/agency/background.js
+++ chrome/agency/background.js
@@ -1,20 +1,3 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function (tab) {
-  // No tabs or host permissions needed!
-  console.log('Turning ' + JSON.stringify({ name: "John Rambo", time: "2pm" }) + ' red!');
-  var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
-  xmlhttp.open("POST", "http://localhost:9090/");
-  xmlhttp.send(tab.url);
-  chrome.tabs.executeScript({
-    code: 'document.body.style.backgroundColor="red"'
-  });
-});
-
-
 chrome.tabs.onActivated.addListener(function (activeInfo) {
   chrome.tabs.get(activeInfo.tabId, function (tab) {
 
