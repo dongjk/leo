52829a75c16bb6fcb4eda68f81f41bac0f7683a8
Jiankang Dong <Jiankang Dong>
2016-12-03 22:25:10 +0800

    inputer works

--- core/cmd/inputer/leo.go
+++ core/cmd/inputer/leo.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"os"
 	"time"
-
+	"fmt"
 	"github.com/dongjk/leo/core/pkg/storage"
 )
 
+type keyInfo struct {
+	Label string
+	time  int64
+}
 
-func main(){
-    
+func main() {
+	ds, err := storage.ConstructDataStore()
+	if err !=nil{
+		fmt.Printf("connect DB error， %v", err)
+	}
+	time := time.Now().UnixNano()
+
+	for _, arg := range os.Args[1:] {
+		ds.Insert("keyword", &keyInfo{arg, time})
+	}
+
 }
